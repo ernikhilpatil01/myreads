aac2daa7972f9ef1850d9f3ec85a6682fc33ce16
Nikhi Patil <nikhil.patil.sbg@gmail.com>
2022-11-17 22:41:16 +0530

    feat: BooksAPI utilized to fetch books and display them in their respective shelf.
    This commit solves the problem of fetching the data and putting the books in their respective shelf.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from 'react-router-dom';
 import "./App.css";
 import Handler from "./components/Handler";
+import * as BooksAPI from "./BooksAPI";
 
 // TODO: add function comments here
 function App() {
@@ -9,18 +10,37 @@ function App() {
   const [wantToReadBooks, setWantToReadBooks] = useState([]);
   const [readBooks, setReadBooks] = useState([]);
 
+  useEffect(() => {
+    let fetchBooks = true;
+    const getAllBooks = async () =>{
+      console.log("fetch api called");
+      if(fetchBooks){
+        await BooksAPI.getAll().then(books=>{
+          //to print the books
+          //books.filter((book)=>console.log(Object.values(book).length));
+          setWantToReadBooks([books.filter((book)=> Object.values(book)[Object.values(book).length-1] === 'wantToRead')]);
+          setReadBooks([books.filter((book)=> Object.values(book)[Object.values(book).length-1] === 'read')]);
+          setCurrentlyReadingBooks([books.filter((book)=> Object.values(book)[Object.values(book).length-1] === 'currentlyReading')]);
+        });
+      }
+    }
+
+    getAllBooks();
+    return () => {
+      fetchBooks = false;
+    }
+  }, [])
+
   return (
-    
-      <Routes>
-        <Route exact path='/' element={
-          <Handler
-            currentlyReadingBooks={currentlyReadingBooks}
-            wantToReadBooks={wantToReadBooks}
-            readBooks={readBooks}
-          />
-        } />
-      </Routes>
-    
+    <Routes>
+      <Route exact path='/' element={
+        <Handler
+          currentlyReadingBooks={currentlyReadingBooks}
+          wantToReadBooks={wantToReadBooks}
+          readBooks={readBooks}
+        />
+      } />
+    </Routes>
   );
 }
 
